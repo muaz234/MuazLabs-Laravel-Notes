f978c9537a1733d936467e73ec3441f14fc2b63b
muaz.ahmed <ahmedmuaz0152@gmail.com>
2019-06-15 10:43:14 +0800

    add model,routes and add action in controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DateTime;
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+
+        return view('notes.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'points' => 'required'
+            'posted' => 'required'
+        ])
+        $now = new Datetime;
+        return view('note')->with('time', $now);
+        $new = new Note([
+            'title' => $request->get('title'),
+            'points' => $request->get('points'),
+            'posted' => $request->get('posted')
+        ]);  
+        $new->save();
+        return redirect('/notes')->with('success', 'New notes successfully saved!.');      
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Note.php
+++ app/Note.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     //
+
+    protected $fillable= [
+        'title',
+        'posted',
+        'points'
+    ]
 }
