1983a30f9cfbad3b6094ed1253346376cc700709
muaz <ahmedmuaz0152@gmail.com>
2019-08-13 23:21:24 +0800

    modify table css

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -15,45 +15,50 @@
         </div>
 
         <div class='row' style="margin-top: 50px; justify-content: center;">
-            <div class="col-md-3">
+            <div class="col-md-1">
 
             </div>
 
 
-            <div class="col-md-6">
-                <table class="table table-hover">
-                    <thead>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Posted On</th>
-                        <th>Timestamps</th>
-                        <th>Actions</th>
-                    </thead>
+            <div class="col-md-10">
+                <div class="table-responsive">
+                    <table class="table table-bordered" style="width: 100% !important;">
+                        <thead>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Posted On</th>
+                            <th>Timestamps</th>
+                            <th>Actions</th>
+                            <th></th>
+                        </thead>
 
-                    <tbody>
-                    @foreach($notes as $note)
-                    <tr>
-                        <td>{{$note->id}}</td>
-                        <td>{{$note->title}}</td>
-                        <td style="margin-left:5px;">{{$note->points}}</td>
-                        <td style="margin-left:5px;">{{$note->posted}}</td>
-                        <td>{{$note->created_at}}</td>
-                        <td><a href="{{route('notes.edit', $note->id)}}"><button type="button" class="btn btn-info">Edit</button></a>
-
-                        <form method="post" action="{{route('notes.destroy', $note->id)}}">
-                                @csrf
-                                @method('DELETE')
-                                    <button type="submit" class="delete btn btn-danger" style="margin-top: 5px;">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        <tbody>
+                        @foreach($notes as $note)
+                            <tr>
+                                <td style="vertical-align: middle;">{{$note->id}}</td>
+                                <td style="vertical-align: middle;">{{$note->title}}</td>
+                                <td style="margin-left:5px; vertical-align: middle;">{{$note->points}}</td>
+                                <td style="margin-left:5px; vertical-align: middle;">{{$note->posted}}</td>
+                                <td style="color:blue; vertical-align: middle;">{{$note->created_at}}</td>
+                                <div class="btn-group">
+                                <td class="text-center"><a href="{{route('notes.edit', $note->id)}}"><button type="button" class="btn btn-info btn-block">Edit</button></a></td>
+                                <td>
+                                        <form method="post" action="{{route('notes.destroy', $note->id)}}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="delete btn btn-danger btn-block">Delete</button>
+                                    </form>
+                                </td>
+                            </div>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-1">
             </div>
         </div>
     @endsection
